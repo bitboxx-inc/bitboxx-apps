0a3d317f823b10017c936be61ef136a2c442aee2
digiot <digiot.abc@gmail.com>
2025-01-25 01:44:32 +0900

    fix privacy policy link.

--- src/routes/odd-or-even/+page.svelte
+++ src/routes/odd-or-even/+page.svelte
@@ -3,6 +3,7 @@
 
     const appId = '6740538794'
     const appName = '奇数偶数どっち'
+    const privacyPolicy = 'https://github.com/bitboxx-inc/bitboxx-apps/blob/main/privacy-policies/%E5%A5%87%E6%95%B0%E5%81%B6%E6%95%B0%E3%81%A9%E3%81%A3%E3%81%A1.md';
 
     function openAppPage(appId: string) {
         window.open(`https://itunes.apple.com/jp/app/id${appId}`, "_blank"); // 新しいタブで開く
@@ -14,7 +15,7 @@
 <p>
     カンタン入力で、奇数か偶数かすぐわかる！
 </p>
-<a href="https://github.com/bitboxx-inc/bitboxx-apps/blob/main/privacy-policy/%E5%A5%87%E6%95%B0%E5%81%B6%E6%95%B0%E3%81%A9%E3%81%A3%E3%81%A1.md" target="_blank">プライバシーポリシー</a>
+<a href={privacyPolicy} target="_blank">プライバシーポリシー</a>
 
 <img class="mt-4 pointer"
      src="/JP/Download_on_App_Store/Black_lockup/SVG/Download_on_the_App_Store_Badge_JP_RGB_blk_100317.svg"
